b8b97e26922a17482b0fcf40ec9712d87e98b2af
Jesus Paye <2113010211@untels.edu.pe>
2024-04-15 15:01:05 -0500

    Pregunta01  - Listo por Paye

---
+++ Preg01.py
@@ -0,0 +1,15 @@
+#       Ejercicio 1
+
+# Intercambiar el valor de 2 variables
+# Ingresamos los datos de 'a' y 'b'
+a = float(input("\nIngresar el valor de 'a' -->:  "))
+b = float(input("Ingresar el valor de 'b' -->:  "))
+
+#CON ESTE METODO SE CAMBIA LOS VALORES
+a , b = b , a
+
+# Imprimimos los nuevos valores
+print("\nEl nuevo valor de 'a' es: " , a)
+print("El nuevo valor de 'b' es: " , b )
+
+print("-------------------------------------------------")
